c81d573fd0c0b4c970f575f3d11b0941c753ba42
Dummy <neetugupta.jkctc@gmail.com>
2021-07-28 17:16:29 +0530

    jwt password encypt

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const User = require('./models/users');
+const bodyParser = require('body-parser');
+// act like middleware
+const jsonParser = bodyParser.json();
+var crypto = require('crypto');
+let key = "password";
+let algo = 'aes256';
+
+mongoose.connect("mongodb+srv://mongo_user:0W5JrhO8PEzmWRNg@cluster0.ptwwm.mongodb.net/node_mongo?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.warn("connected");
+});
+
+// app.get('/', function (req, res) {
+//     res.end("hello");
+// })
+
+app.post('/register', jsonParser, function (req, res) {
+    var cipher = crypto.createCipheriv(algo, key);
+    // var cipher = crypto.createCipher(algo, key);
+    var encrypted = cipher.update(req.body.password, 'utf8', 'hex');
+    +cipher.final('hex');
+    
+    const data = new User({
+        _id : new mongoose.Types.ObjectId,
+        name: req.body.name,
+        email: req.body.email,
+        address: req.body.address,
+        password: encrypted
+    });
+
+    data.save().then((result) => {
+        res.status(201).json(result)
+    }).catch((err) => console.warn(err));
+})
+
+app.listen(4000);
